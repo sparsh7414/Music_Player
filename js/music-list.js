fd773d46a8268a8572e69915f34f6ff2562c3f0f
Sparsh Bhardwaj <sparsh.Bhardwaj@zimozi.co>
2021-11-15 11:09:55 +0530

    First Commit

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic=[
+    {
+        name:"Relation",
+        artist:"Nikk",
+        img:"Tere_mere",
+        src:"Relation"
+    },
+    {
+        name:"Shoot da order",
+        artist:"Jass Manak, Jagpal Sandhu",
+        img:"shoot_da_order",
+        src:"Shoot da order"
+    },
+    {
+        name:"Raataan Lambiyan",
+        artist:"Jubin Nautiyal, Tanishk Bagchi",
+        img:"Raataan Lambiyan",
+        src:"Raataan Lambiyan"
+    },
+    {
+        name:"Mera Mann Kehne Laga",
+        artist:"Falak Shabir",
+        img:"Mera Mann Kehne Laga",
+        src:"Mera Mann Kehne Laga"
+    },
+    {
+        name:"Yaari",
+        artist:"Nikk",
+        img:"Yaari",
+        src:"Yaari"
+    },
+    {
+        name:"Tenu Na Bol Pawaan",
+        artist:"Jyotica Tangri, Yasser Desai",
+        img:"Tenu Na Bol Pawaan",
+        src:"Tenu Na Bol Pawaan"
+    }
+]
---
+++ js/script.js
@@ -0,0 +1,152 @@
+const wrapper= document.querySelector(".wrapper"),
+musicImg=wrapper.querySelector(".img-area img"),
+musicName=wrapper.querySelector(".song-details .name"),
+musicArtist=wrapper.querySelector(".song-details .artist");
+
+let musicIndex=2;
+
+window.addEventListener("load",()=>{
+    loadMusic(musicIndex);
+    playingSong(); 
+
+})
+
+function loadMusic()
+{
+    musicName.innerText = allMusic[indexNumb - 1].name;
+    musicArtist.innerText = allMusic[indexNumb - 1].artist;
+    musicImg.src = `images/${allMusic[indexNumb - 1].src}.jpg`;
+    mainAudio.src = `music/${allMusic[indexNumb - 1].src}.mp3`;
+
+}
+
+function playMusic()
+{
+    wrapper.classList.add("paused");
+    playPauseBtn.querySelector("i").innerText = "pause";
+    mainAudio.play();
+}
+
+function pauseMusic()
+{
+    wrapper.classList.remove("paused");
+    playPauseBtn.querySelector("i").innerText = "play_arrow";
+    mainAudio.pause();
+}
+
+function prevMusic()
+{
+    musicIndex--; 
+    musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;
+    loadMusic(musicIndex);
+    playMusic();
+    playingSong(); 
+}
+
+function nextMusic()
+{
+    musicIndex++; //increment of musicIndex by 1
+    //if musicIndex is greater than array length then musicIndex will be 1 so the first music play
+    musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;
+    loadMusic(musicIndex);
+    playMusic();
+    playingSong(); 
+}
+
+playPauseBtn.addEventListener("click", ()=>{
+    const isMusicPlay = wrapper.classList.contains("paused");
+    //if isPlayMusic is true then call pauseMusic else call playMusic
+    isMusicPlay ? pauseMusic() : playMusic();
+    playingSong();
+});
+
+prevBtn.addEventListener("click", ()=>{
+    prevMusic();
+});
+
+nextBtn.addEventListener("click", ()=>{
+    nextMusic();
+});
+
+mainAudio.addEventListener("timeupdate", (e)=>{
+    const currentTime = e.target.currentTime; //getting playing song currentTime
+    const duration = e.target.duration; //getting playing song total duration
+    let progressWidth = (currentTime / duration) * 100;
+    progressBar.style.width = `${progressWidth}%`;
+    
+    let musicCurrentTime = wrapper.querySelector(".current-time"),
+    musicDuartion = wrapper.querySelector(".max-duration");
+    mainAudio.addEventListener("loadeddata", ()=>{
+      // update song total duration
+      let mainAdDuration = mainAudio.duration;
+      let totalMin = Math.floor(mainAdDuration / 60);
+      let totalSec = Math.floor(mainAdDuration % 60);
+      if(totalSec < 10){ //if sec is less than 10 then add 0 before it
+        totalSec = `0${totalSec}`;
+      }
+      musicDuartion.innerText = `${totalMin}:${totalSec}`;
+    });
+    let currentMin = Math.floor(currentTime / 60);
+    let currentSec = Math.floor(currentTime % 60);
+    if(currentSec < 10){ //if sec is less than 10 then add 0 before it
+      currentSec = `0${currentSec}`;
+    }
+    musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
+});
+
+progressArea.addEventListener("click", (e)=>{
+    let progressWidth = progressArea.clientWidth; //getting width of progress bar
+    let clickedOffsetX = e.offsetX; //getting offset x value
+    let songDuration = mainAudio.duration; //getting song total duration
+    
+    mainAudio.currentTime = (clickedOffsetX / progressWidth) * songDuration;
+    playMusic(); //calling playMusic function
+    playingSong();
+});
+
+const repeatBtn = wrapper.querySelector("#repeat-plist");
+repeatBtn.addEventListener("click", ()=>{
+  let getText = repeatBtn.innerText; //getting this tag innerText
+  switch(getText){
+    case "repeat":
+      repeatBtn.innerText = "repeat_one";
+      repeatBtn.setAttribute("title", "Song looped");
+      break;
+    case "repeat_one":
+      repeatBtn.innerText = "shuffle";
+      repeatBtn.setAttribute("title", "Playback shuffled");
+      break;
+    case "shuffle":
+      repeatBtn.innerText = "repeat";
+      repeatBtn.setAttribute("title", "Playlist looped");
+      break;
+  }
+});
+
+//code for what to do after song ended
+mainAudio.addEventListener("ended", ()=>{
+    // we'll do according to the icon means if user has set icon to
+    // loop song then we'll repeat the current song and will do accordingly
+    let getText = repeatBtn.innerText; //getting this tag innerText
+    switch(getText){
+      case "repeat":
+        nextMusic(); //calling nextMusic function
+        break;
+      case "repeat_one":
+        mainAudio.currentTime = 0; //setting audio current time to 0
+        loadMusic(musicIndex); //calling loadMusic function with argument, in the argument there is a index of current song
+        playMusic(); //calling playMusic function
+        break;
+      case "shuffle":
+        let randIndex = Math.floor((Math.random() * allMusic.length) + 1); //genereting random index/numb with max range of array length
+        do{
+          randIndex = Math.floor((Math.random() * allMusic.length) + 1);
+        }while(musicIndex == randIndex); //this loop run until the next random number won't be the same of current musicIndex
+        musicIndex = randIndex; //passing randomIndex to musicIndex
+        loadMusic(musicIndex);
+        playMusic();
+        playingSong();
+        break;
+    }
+  });
+  
